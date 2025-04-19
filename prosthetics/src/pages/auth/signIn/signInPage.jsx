286bf7fb14b60870f543e9104b9c80527b4a5a86
nazarSHP99 <145098692+nazarSHP99@users.noreply.github.com>
2025-04-19 13:41:04 +0300

    feat: signIn page

--- prosthetics/src/pages/auth/signIn/signInPage.jsx
+++ prosthetics/src/pages/auth/signIn/signInPage.jsx
@@ -1,18 +1,24 @@
+import React from 'react'
+import './signInStyles.css'
+
 export default function SignInPage() {
   return (
     <div className="sign-in-page">
-      <h1>Sign In</h1>
-      <form>
-        <label>
-          Email
-          <input type="email" />
-        </label>
-        <label>
-          Password
-          <input type="password" />
-        </label>
-        <button type="submit">Sign In</button>
-      </form>
+      <div className="sign-in-form-container">
+        <h1>Вхід до кабінету</h1>
+        <form className='sign-in-form'>
+          <label>
+            Логін
+            <input type="email" placeholder="Введіть електронну пошту" />
+          </label>
+          <label>
+            Пароль
+            <input type="password" placeholder="Введіть пароль до кабінету" />
+          </label>
+          <button type="submit">Ввійти</button>
+        </form>
+        <p className="sign-in-note">Не зареєстровані? <a href="/singUp">Зробіть це зараз</a></p>
+      </div>
     </div>
   )
 }
